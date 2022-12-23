49b98095961817619dcedfaaf4219ea106ce43ab
Helge <helge.krueger@gmail.com>
2022-12-23 12:17:46 +0100

    Correctly getting followers from various instances

--- mastodon.py
+++ mastodon.py
@@ -3,115 +3,67 @@ import requests
 import os
 import re
 
+from rich.progress import Progress
+
 
 class MastodonClient:
-    def __init__(self):
-        self.config = self._load_config()
+    def __init__(self, username):
+        acct, instance = username.split("@")
 
-        self.instance = self.config["instance"]
-        if "access_token" in self.config:
-            self.access_token = self.config["access_token"]
+        self.acct = acct
+        self.instance = instance
 
-    def _load_config(self):
-        if os.path.exists("config_mastodon.yaml"):
-            with open("config_mastodon.yaml", "r") as f:
-                return yaml.safe_load(f)
-        else:
-            return self.register()
-
-    def register(self):
-        self.instance = input("What instance do you want to use: ")
-
-        if self.instance.startswith("https://"):
-            self.instance = self.instance[8:]
-
-        post_data = {
-            "client_name": "Follow recommendations",
-            "redirect_uris": "urn:ietf:wg:oauth:2.0:oob",
-            "scopes": "read",
-            "website": "http://127.0.0.1",
-        }
-
-        result = requests.post(f"https://{self.instance}/api/v1/apps", data=post_data)
-
-        data = result.json()
-        client_id = data["client_id"]
-        client_secret = data["client_secret"]
-
-        config = {
-            "instance": self.instance,
-            "client_id": client_id,
-            "client_secret": client_secret,
-        }
-
-        with open("config_mastodon.yaml", "w") as f:
-            f.write(yaml.dump(config))
-
-        return config
-
-    def _save_config(self):
-        with open("config_mastodon.yaml", "w") as f:
-            f.write(yaml.dump(self.config))
-
-    def _default_headers(self):
-        return {"Authorization": f"Bearer {self.access_token}"}
-
-    def get_url_for_code(self):
-        client_id = self.config["client_id"]
-
-        return f"https://{self.instance}/oauth/authorize?client_id={client_id}&scope=read&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code"
-
-    def use_code(self, code):
-        post_data = {
-            "client_id": self.config["client_id"],
-            "client_secret": self.config["client_secret"],
-            "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
-            "grant_type": "authorization_code",
-            "code": code,
-            "scope": "read",
-        }
-        response = requests.post(f"https://{self.instance}/oauth/token", data=post_data)
-
-        self.access_token = response.json()["access_token"]
-
-        self.config["access_token"] = self.access_token
-        self._save_config()
-
-    def get_user(self):
-        resp = requests.get(
-            f"https://{self.instance}/api/v1/accounts/verify_credentials",
-            headers=self._default_headers(),
-        )
-
-        return {"id": resp.json()["id"], "name": resp.json()["display_name"]}
-
-    def get_following(self, account_id):
+    def get_following(self, account_id, acct):
         following = []
 
-        urls_to_check = [
-            f"https://{self.instance}/api/v1/accounts/{account_id}/following"
-        ]
+        # print(acct)
+        if "@" in acct:
+            username, instance = acct.split("@")
+        else:
+            username = acct
+            instance = self.instance
+
+        resp = requests.get(
+            f"https://{instance}/api/v1/accounts/lookup", params={"acct": username}
+        )
+
+        if not resp.ok:
+            return []
+
+        account_id = resp.json()["id"]
+
+        print(f"Retrieved account_id {account_id} for {acct} on {instance}")
+
+        urls_to_check = [f"https://{instance}/api/v1/accounts/{account_id}/following"]
         urls_checked = []
 
-        print(f"Retrieving followers for Account id {account_id}")
+        print(f"Retrieving people being followed by Account {acct}")
 
-        while len(urls_to_check) > 0:
-            url = urls_to_check.pop()
-            urls_checked.append(url)
+        with Progress() as progress:
+            task_id = progress.add_task(f"Fetching people {acct} follows")
+            while len(urls_to_check) > 0:
+                url = urls_to_check.pop()
+                urls_checked.append(url)
 
-            resp = requests.get(
-                url,
-                headers=self._default_headers(),
-            )
+                resp = requests.get(url)
 
-            following_ids = [x["id"] for x in following]
-            following += [x for x in resp.json() if x["id"] not in following_ids]
+                following_ids = [x["id"] for x in following]
+                following += [
+                    x
+                    for x in resp.json()
+                    if type(x) == dict and x["id"] not in following_ids
+                ]
 
-            if "link" in resp.headers:
-                links = resp.headers["link"].split(",")
-                links = [re.search(r"<(.*?)>", l)[1] for l in links]
-                urls_to_check += [l for l in links if l not in urls_checked]
+                progress.update(
+                    task_id,
+                    description=f"...Retrieved {len(following)} people being followed by account {acct}",
+                )
 
-        print(f"Retrieved {len(following)} followers for Account id {account_id}")
+                if "link" in resp.headers:
+                    links = resp.headers["link"].split(",")
+                    links = [re.search(r"<(.*?)>", l)[1] for l in links]
+                    urls_to_check += [l for l in links if l not in urls_checked]
+
+        print(f"Retrieved {len(following)} people being followed by account {acct}")
 
         return following
--- recommendations_to_follow.py
+++ recommendations_to_follow.py
@@ -7,6 +7,9 @@ from database import Suggestion, Following
 
 from rich import print
 from rich.table import Table
+from rich.prompt import Prompt
+
+from datetime import date, timedelta
 
 from argparse import ArgumentParser
 
@@ -14,23 +17,22 @@ parser = ArgumentParser()
 parser.add_argument("--update_own_followers", action="store_true", default=False)
 args = parser.parse_args()
 
-mc = MastodonClient()
 
-if "access_token" not in mc.config:
-    print("Please visit ", mc.get_url_for_code())
-    code = input("Please enter code: ")
-    mc.use_code(code)
-
-account_id = mc.get_user()["id"]
+account_id = Prompt.ask(
+    "Please enter your [bold]mastodon username[/bold] (e.g. [italic]helgek@mas.to[/italic])"
+)
 
 
-def get_following(account_id, force_update=False):
+mc = MastodonClient(account_id)
+
+
+def get_following(account_id, acct, force_update=False):
     data, created = Following.get_or_create(account_id=account_id)
 
     if not created and not force_update:
         return data
 
-    following = mc.get_following(account_id)
+    following = mc.get_following(account_id, acct)
     data.data = json.dumps(following)
     data.save()
 
@@ -42,26 +44,32 @@ suggestions = defaultdict(int)
 lookup = {}
 
 following = json.loads(
-    get_following(account_id, force_update=args.update_own_followers).data
+    get_following(account_id, account_id, force_update=args.update_own_followers).data
 )
-following_ids = [x["id"] for x in following]
+following_ids = [(x["id"], x["acct"]) for x in following]
 
-for acc_id in following_ids:
-    dd = get_following(acc_id)
-    for x in json.loads(dd.data):
-        suggestions[x["id"]] += 1
-        lookup[x["id"]] = x
+for acc_id, acct in following_ids:
+    print()
+    print(f"Local Account ID {acc_id} for {acct}")
+
+    dd = get_following(acc_id, acct)
+    try:
+        for x in json.loads(dd.data):
+            suggestions[x["acct"]] += 1
+            lookup[x["acct"]] = x
+    except:
+        print(f"Something is wrong with data for {acct}")
 
 suggestion_list = [
-    lookup[x]
+    (cc, lookup[x])
     for x, cc in suggestions.items()
     if cc > 3
-    if x not in following_ids + [account_id]
+    if x not in [y[1] for y in following_ids] + [account_id]
 ]
 
 suggestion_list = sorted(
     suggestion_list,
-    key=lambda x: x["last_status_at"] if x["last_status_at"] else "",
+    key=lambda x: x[0],
     reverse=True,
 )
 
@@ -71,17 +79,35 @@ table = Table(title="Suggested people to follow")
 table.add_column("Display name")
 table.add_column("Last Status Update")
 table.add_column("url")
+table.add_column("Followed by")
 
 suggestions_left = 5
 
-for user in suggestion_list:
 
+def should_display_suggestion(user):
+    if not user["discoverable"]:
+        return False
+
+    try:
+        last_status = date.fromisoformat(user["last_status_at"])
+    except:
+        return False
+
+    if date.today() - last_status > timedelta(days=3):
+        return False
+
+    return True
+
+
+for count, user in suggestion_list:
     suggestion, created = Suggestion.get_or_create(account_id=user["id"])
 
-    # if user["discoverable"]:
-    if created:
-        table.add_row(user["display_name"], user["last_status_at"], user["url"])
-        suggestions_left -= 1
+    if should_display_suggestion(user):
+        if created:
+            table.add_row(
+                user["display_name"], user["last_status_at"], user["url"], f"{count}"
+            )
+            suggestions_left -= 1
 
     if suggestions_left == 0:
         break
