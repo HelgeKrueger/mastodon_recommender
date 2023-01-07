fa493332694d1bb728e43485525de46364953d37
Helge <helge.krueger@gmail.com>
2023-01-07 13:19:16 +0100

    Sort hashtags alphabetically

--- webapp/src/HashtagChoice.js
+++ webapp/src/HashtagChoice.js
@@ -14,15 +14,9 @@ const HashtagItem = ({ tag }) => {
 const HashtagChoice = ({ hashtags, chosen, setChosen }) => {
   //   const [chosen, setChosen] = useState([]);
 
-  let hashtagGroups = [];
-
   const itemsPerRow = 200;
 
-  for (let j = 0; j < hashtags.length / itemsPerRow; j++) {
-    hashtagGroups.push(
-      hashtags.slice(j * itemsPerRow, j * itemsPerRow + itemsPerRow)
-    );
-  }
+  hashtags = hashtags.sort();
 
   const handleChange = (event, value) => {
     setChosen(value);
@@ -31,23 +25,19 @@ const HashtagChoice = ({ hashtags, chosen, setChosen }) => {
   return (
     <Box sx={{ backgroundColor: "white" }}>
       <Typography variant="h4">Choose Hashtags you like</Typography>
-      {hashtagGroups.map((group, idx) => {
-        return (
-          <ToggleButtonGroup
-            color="primary"
-            key={idx}
-            value={chosen}
-            onChange={handleChange}
-            sx={{ display: "block" }}
-          >
-            {group.map((tag) => (
-              <ToggleButton key={tag} value={tag}>
-                {tag}
-              </ToggleButton>
-            ))}
-          </ToggleButtonGroup>
-        );
-      })}
+
+      <ToggleButtonGroup
+        color="primary"
+        value={chosen}
+        onChange={handleChange}
+        sx={{ display: "block" }}
+      >
+        {hashtags.map((tag) => (
+          <ToggleButton key={tag} value={tag}>
+            {tag}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
     </Box>
   );
 };
